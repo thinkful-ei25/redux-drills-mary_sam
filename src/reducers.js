cf803a8f87567f04fce41a1c73aa5c4712508dc4
Sam Dalton <spd4nf@virginia.edu>
2018-11-13 10:50:34 -0600

    firist commit

---
+++ src/reducers.js
@@ -0,0 +1,38 @@
+import {
+    SEARCH_CHARACTERS_REQUEST,
+    SEARCH_CHARACTERS_SUCCESS,
+    SEARCH_CHARACTERS_ERROR
+} from './actions';
+
+const initialState = {
+    characters: ['Jar Jar Binks'],
+    loading: false,
+    error: null
+};
+
+export function characterReducer(state=initialState, action) {
+    // Handle these sync actions
+    if (action.type === SEARCH_CHARACTERS_REQUEST) {
+        return {
+            ...state,
+            loading: true,
+            error: null
+        };
+    }
+    else if (action.type === SEARCH_CHARACTERS_SUCCESS) {
+        return {
+            ...state,
+            characters: action.characters,
+            loading: false,
+            error: null
+        };
+    }
+    else if (action.type === SEARCH_CHARACTERS_ERROR) {
+        return {
+            ...state,
+            loading: false,
+            error: action.error
+        };
+    }
+    return state;
+}
